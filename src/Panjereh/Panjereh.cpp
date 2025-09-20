51f5e51ac44269faf20988589614cf8aa90b20c3
r.pouya <rpouya@hotmail.com>
2025-09-20 10:20:52 +0330

    all dependenceis added

--- src/Panjereh/Panjereh.cpp
+++ src/Panjereh/Panjereh.cpp
@@ -1,7 +1,15 @@
 // Panjereh.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
 #include <iostream>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 int main()
 {
