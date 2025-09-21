0b4a48c5ceb2605957979b393b1358c529a49b18
r.pouya <rpouya@hotmail.com>
2025-09-21 11:48:57 +0330

    add new project

---
+++ src/Renderer/OpenGlRenderer.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <cstdint>
+
+#define GL_SILENCE_DEPRECATION
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+
+
+class OpenGLRenderer {
+public:
+
+
+private:
+
+
+
+};
