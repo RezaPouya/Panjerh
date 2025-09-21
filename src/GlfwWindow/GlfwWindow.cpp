47855f8231fe3ae80734b522c37bfffc7c4c30ad
RezaPouya <rpouya@hotmail.com>
2025-09-21 23:32:04 +0330

    update

--- src/GlfwWindow/GlfwWindow.cpp
+++ src/GlfwWindow/GlfwWindow.cpp
@@ -3,6 +3,19 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/gl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main()
 {
@@ -14,12 +27,14 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use Core Profile
 
 	GLFWwindow* window = glfwCreateWindow(1024, 760, "Hello world", nullptr, nullptr);
-
+	
 	if (window == nullptr) {
 		glfwTerminate(); // Clean up GLFW before throwing
 		throw std::runtime_error("Failed to create GLFW window");
 	}
 
+	glfwSetErrorCallback(error_callback);
+	
 	glfwMakeContextCurrent(window);
 
 	// 3. Initialize GLAD
@@ -27,6 +42,8 @@ int main()
 		glfwTerminate();
 		throw std::runtime_error("Failed to initialize GLAD");
 	}
+
+	glfwSetKeyCallback(window, key_callback);
 
 	while (!glfwWindowShouldClose(window))
 	{
